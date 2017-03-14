0ab03673b1cdba71437f2ec42bb3993440dd433f
Oleg Martynenko <1ole4g5@gmail.com>
2017-03-14 18:27:30 +0300

    Added home work for task2

---
+++ sandbox/src/main/java/ru/stqa/jpfste/sandbox/MyFirstProgram.java
@@ -0,0 +1,15 @@
+package ru.stqa.jpfste.sandbox;
+
+public class MyFirstProgram {
+	public static void main(String[] args) {
+		Point p1 = new Point(1, 2);
+		Point p2 = new Point(3, 6);
+
+		System.out.println("The distance between two points (using function): = " + getDistanceBetweenTwoPoints(p1, p2));
+		System.out.println("The distance between two points (using method): = " + p1.getDistanceBetweenTwoPoints2(p2));
+	}
+
+	public static double getDistanceBetweenTwoPoints(Point p1, Point p2) {
+		return Math.sqrt(Math.pow((p1.y - p1.x), 2) + Math.pow((p2.y - p2.x), 2));
+	}
+}
---
+++ sandbox/src/main/java/ru/stqa/jpfste/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.jpfste.sandbox;
+
+public class Point {
+	public double x;
+	public double y;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getDistanceBetweenTwoPoints2(Point p) {
+		return Math.sqrt(Math.pow((this.y - this.x), 2) + Math.pow((p.y - p.x), 2));
+	}
+}
