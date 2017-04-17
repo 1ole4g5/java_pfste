34ed321d22d94ffcca28f09ee1ee193d8fe7eeed
Oleg Martynenko <1ole4g5@gmail.com>
2017-04-17 21:42:23 +0300

    Fixed mistakes for home work task11 second part

--- addressbook/src/test/java/ru/stqa/jpfste/addressbook/tests/ContactDisplayDataFormTests.java
+++ addressbook/src/test/java/ru/stqa/jpfste/addressbook/tests/ContactDisplayDataFormTests.java
@@ -16,9 +16,9 @@ public class ContactDisplayDataFormTests extends TestBase {
 	@BeforeMethod
 	public void ensurePreconditions() {
 		if (app.contact().all().size() == 0) {
-			app.contact().create(new ContactData().withFirstName("First2 name").withLastName("Last name2")
+			app.contact().create(new ContactData().withFirstName("First name").withLastName("Last name")
 			        .withAddress("789-)01").withFirstEMail("test@test.com").withSecondEMail("1@1").withThirdEMail("1+1")
-			        .withHomePhone("+7(111)").withMobilePhone("2-2-2").withWorkPhone("3 3 3"));//, false);
+			        .withHomePhone("+7(111)").withMobilePhone("2-2-2").withWorkPhone("3 3 3").withGroup("new group"));
 		}
 	}
 
