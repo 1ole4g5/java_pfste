b1c59a82b57082c91db966ff75f649f5ef768886
Oleg Martynenko <1ole4g5@gmail.com>
2017-05-12 19:27:45 +0300

    Fixed mistakes for home work task23

--- addressbook/src/test/java/ru/stqa/jpfste/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/jpfste/addressbook/appmanager/ApplicationManager.java
@@ -32,7 +32,7 @@ public class ApplicationManager {
 
 	public void init() throws FileNotFoundException, IOException {
 		String target = System.getProperty("target", "local");
-		properties.load(new FileReader(new File(String.format("addressbook/src/test/resources/%s.properties", target))));
+		properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 	}
 
 	public void stop() {
